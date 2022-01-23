532955865dc473e6f2bd825100d515ffa21c4f0e
Stuart Bowman <bowman.d.stuart@gmail.com>
2022-01-23 15:47:06 -0500

    added query_utils.py

---
+++ src/query_utils.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import re
+
+global API_KEY
+API_URL = 'https://api.esv.org/v3/passage/text/'
+
+def process_secrets(secrets_file:str=None):
+    with open(secrets_file) as file:
+        secrets_contents = json.load(file)
+
+        global API_KEY
+        API_KEY = secrets_contents['token']
+
+def create_passage_query(book:str=None, chapter:str=None, verse:str=None):
+    '''
+    '''
+    return f'{book} {chapter}:{verse}'
+
+
+def get_esv_text(passage):
+    params = {
+        'q': passage,
+        'indent-poetry': False,
+        'include-headings': False,
+        'include-footnotes': False,
+        'include-verse-numbers': False,
+        'include-short-copyright': False,
+        'include-passage-references': False
+    }
+
+    headers = {
+        'Authorization': 'Token %s' % API_KEY
+    }
+
+    data = requests.get(API_URL, params=params, headers=headers).json()
+
+    text = re.sub('\s+', ' ', data['passages'][0]).strip()
+
+    return text # '%s – %s' % (text, data['canonical'])
+
